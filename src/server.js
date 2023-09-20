02d1d73d5bc3201eec5f5a30c1d8725350ed47d4
Awalludin Fajar <fajarawalludin25@gmail.com>
2023-09-20 11:25:49 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+        headers: ['Accept', 'Authorization', 'Content-Type', 'If-None-Match', 'Accept-language'], // all default apart from Accept-language
+        additionalHeaders: ['cache-control', 'x-requested-with', 'Access-Control-Allow-Origin'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
